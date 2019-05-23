a5825b32932ea499a3c9ae3b9c7d9f8ab879d455
Chris Parnin <chris.parnin@gmail.com>
2019-05-23 13:00:26 -0400

    Support running from stepfile from cli

--- index.js
+++ index.js
@@ -3,59 +3,40 @@ const path  = require('path');
 const fs    = require('fs');
 const yargs = require('yargs');
 const chalk = require('chalk');
-const child = require('child_process');
-const cheerio = require('cheerio');
-const marked = require('marked');
 
 const Select    = require('./lib/select');
 const Operators = require('./lib/operators');
 const Infra     = require('./lib/infrastructure');
 const Steps     = require('./lib/steps');
+const Parse     = require('./lib/parse');
 
 (async()=>{
 
-    yargs.command('test <documentation> <infra>', 'Test markdown file', (yargs) => { }, async (argv) => {
+    yargs.command('test <stepfile>', 'Test markdown file', (yargs) => { }, async (argv) => {
 
-        let markdown = fs.readFileSync( argv.documentation ).toString();
-        let infra = argv.infra;
         let kind = 'slim';
-
-        // Set options
-        marked.setOptions({
-            renderer: new marked.Renderer(),
-            pedantic: false,
-            gfm: true,
-            tables: true,
-            breaks: false,
-            sanitize: false,
-            smartLists: true,
-            smartypants: false,
-            xhtml: false
-        });
-
-        // Compile
-        let html = await marked(markdown);
-
-        console.log(chalk`{bold Translated markdown into html:}`)
-        console.log(chalk`{gray ${html}}`);
-
-        const $ = cheerio.load(html)
 
         // Headless infrastructure (slim)
         //let conn = Connector.getConnector('local');
         // to be replaced by better infra.connectors
-        let conn = await Infra.setup('slim', infra, 'phpx');
+        let conn = await Infra.setup('slim', 'tobefixed', 'phpx');
 
         let sl = new Select( new Operators(conn) );
         let stepper = new Steps();
+        let parser   = new Parse();
 
         console.log(chalk`{bold \nRunning documentation tests:\n}`)
 
         // Select/translate/perform/assert workflow
-        let steps = await stepper.read("examples/unix-service/steps.yml");
-        for( let stepFn of steps )
+        let docs = await stepper.read(argv.stepfile);
+        for( let doc of docs )
         {
-            stepFn($,sl);
+            let md = path.join( path.dirname(argv.stepfile), doc.file);
+            let $ = await parser.markdown2HTML(md);
+            for( let stepFn of doc.steps )
+            {
+                await stepFn($,sl);
+            }
         }
 
         // // create php content
