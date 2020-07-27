2d2054311744eacd765d20db1716126bee5ab02a
Chris Parnin <chris.parnin@gmail.com>
2020-07-27 13:10:25 -0400

    fix: Write to stderr when logging errors from commands. Accordingly adjust test.

--- test/test.js
+++ test/test.js
@@ -4,9 +4,12 @@ const spawnSync = require('child_process').spawnSync;
 
 describe('Executing remote instructions', () => {
     test('On local host', () => {
-        let result = spawnSync('node index.js report examples/remote/steps.yml', { shell:true, stdio: 'inherit' });
+        let result = spawnSync('node index.js report examples/remote/steps.yml', { shell:true });
+
+        // console.log(result);
 
         expect(result.error).toBeUndefined();
+        expect(result.stderr.toString()).toHaveLength(0);
     });
 });
 
