4ec28e73ce48aca74933a0be8d99ebd454ff7e60
Chris Parnin <chris.parnin@gmail.com>
2020-07-27 19:09:20 -0400

    use .exitCode

--- test/test.js
+++ test/test.js
@@ -50,7 +50,7 @@ describe('Running basic commands [bakerx/ssh]', () => {
         let result = spawnSync('node index.js report test/resources/commands/ssh-command.yml', { shell:true });
 
         expect(result.error).toBeUndefined();
-        expect(result.exitcode).toEqual(0);
+        expect(result.exitCode).toEqual(0);
     });
 
 
