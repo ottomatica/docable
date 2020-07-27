861a6ab9486dbf9e174bf29989e58bc23406bb1c
Chris Parnin <chris.parnin@gmail.com>
2020-07-27 14:02:31 -0400

    adding basic commands.

---
+++ test/resources/commands/badpipe.md
@@ -0,0 +1,12 @@
+<!--
+setup:
+  local: {}
+-->
+
+# Bad Pipe
+
+We should expect this to be detected as a failure.
+
+```bash|type=command
+foocommand | echo "hello"
+```
--- test/test.js
+++ test/test.js
@@ -2,14 +2,21 @@ const { assert } = require('console');
 
 const spawnSync = require('child_process').spawnSync;
 
-describe('Executing remote instructions', () => {
-    test('On local host', () => {
-        let result = spawnSync('node index.js report examples/remote/steps.yml', { shell:true });
+describe('Running basic commands', () => {
 
-        // console.log(result);
+    test('Should be able to run a simple command', () => {
+        let result = spawnSync('node index.js report test/resources/commands/command.md', { shell:true });
 
         expect(result.error).toBeUndefined();
         expect(result.stderr.toString()).toHaveLength(0);
+    });
+
+
+    test('Should fail on bad commands in pipes', () => {
+        let result = spawnSync('node index.js report test/resources/commands/badpipe.md', { shell:true });
+
+        expect(result.error).toBeUndefined();
+        expect(result.stderr.toString()).not.toHaveLength(0);
     });
 });
 
