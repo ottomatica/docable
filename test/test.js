e8820bf1fed5acab14e60f5e7eca7599e14b0d37
Chris Parnin <chris.parnin@gmail.com>
2020-07-27 15:45:27 -0400

    wip: ssh/tests

--- test/test.js
+++ test/test.js
@@ -27,3 +27,46 @@ describe('Running basic commands [inline]', () => {
     });
 });
 
+// describe('Running basic commands [bakerx/ssh]', () => {
+
+//     const HOST = '192.168.33.10';
+//     let sshConfig = null;
+//     beforeAll(() => {
+//         execSync('bakerx pull ottomatica/bakerx#images bionic-node', {stdio: 'inherit'});
+//         execSync(`bakerx run opunit-test-vm bionic-node --ip ${HOST} --memory 1024 --up test/resources/init.sh`, {stdio: 'inherit'});
+
+//         sshConfig = JSON.parse(execSync(`bakerx ssh-info opunit-test-vm --format json`).toString().trim());
+//     })
+
+//     afterAll(() => {
+//         // delete test vm
+//         execSync('bakerx delete vm opunit-test-vm');
+        
+//         // delete test files
+//         execSync('rm /tmp/foo /tmp/foo.json /tmp/foo.yml /tmp/app.js');
+//     });
+
+//     test('Should be able to run a simple command', () => {
+//         let result = spawnSync('node index.js report test/resources/commands/command.yml', { shell:true });
+
+//         expect(result.error).toBeUndefined();
+//         expect(result.stderr.toString()).toHaveLength(0);
+//     });
+
+
+//     test('Should fail on bad commands in pipes', () => {
+//         let result = spawnSync('node index.js report test/resources/commands/badpipe.yml', { shell:true });
+
+//         expect(result.error).toBeUndefined();
+//         expect(result.stderr.toString()).not.toHaveLength(0);
+//     });
+
+//     test('Should create simple file with content', () => {
+//         let result = spawnSync('node index.js report test/resources/commands/file.yml', { shell:true });
+
+//         expect(result.error).toBeUndefined();
+//         expect(result.stdout.toString()).toMatch('docable');
+//     });
+
+// });
+
